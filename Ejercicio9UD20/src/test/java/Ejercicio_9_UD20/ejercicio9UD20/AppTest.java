7c9fe211191e9d81f04c440c6a2e3b266793919e
AidaQ27 <aidaqueraltt@gmail.com>
2022-05-09 16:08:16 +0200

    fusion carpetas

---
+++ Ejercicio9UD20/src/test/java/Ejercicio_9_UD20/ejercicio9UD20/AppTest.java
@@ -0,0 +1,20 @@
+package Ejercicio_9_UD20.ejercicio9UD20;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
