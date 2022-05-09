65b7e883b8fb9cb92bc65570032896a8fee0f6dc
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-05-09 15:47:39 +0200

    Proyecto ejercicio4

---
+++ Ejercicio4UD20/src/main/java/UD20/Ejercicio4UD20/App.java
@@ -0,0 +1,13 @@
+package UD20.Ejercicio4UD20;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worldasdasdas!" );
+    }
+}
