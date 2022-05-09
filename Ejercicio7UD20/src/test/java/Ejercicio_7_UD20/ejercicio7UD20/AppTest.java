820a5d1f6a60fb2ee68d8c15c0f95b3446e948f7
AidaQ27 <aidaqueraltt@gmail.com>
2022-05-09 14:00:49 +0200

    subiendo todas las carpetas

---
+++ Ejercicio7UD20/src/test/java/Ejercicio_7_UD20/ejercicio7UD20/AppTest.java
@@ -0,0 +1,20 @@
+package Ejercicio_7_UD20.ejercicio7UD20;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
