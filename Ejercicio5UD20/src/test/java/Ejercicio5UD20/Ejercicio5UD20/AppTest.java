b7cb208219d674cfdb36e0f1536c500ac92d4107
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-05-11 17:26:04 +0200

    Merge. ok ej3,4,5,7,8,9

--- Ejercicio5UD20/src/test/java/Ejercicio5UD20/Ejercicio5UD20/AppTest.java
+++ Ejercicio5UD20/src/test/java/Ejercicio5UD20/Ejercicio5UD20/AppTest.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:Ejercicio4UD20/src/test/java/UD20/Ejercicio4UD20/AppTest.java
-package UD20.Ejercicio4UD20;
-=======
 package Ejercicio5UD20.Ejercicio5UD20;
->>>>>>> Ejercicio-5:Ejercicio5UD20/src/test/java/Ejercicio5UD20/Ejercicio5UD20/AppTest.java
 
 import static org.junit.Assert.assertTrue;
 
